e7e35f2d9e668d24461650d7e1a86a32abdc1a1e
marla294@gmail.com <marla294@gmail.com>
2022-06-08 13:51:22 -0500

    setting back to use the heroku thing

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
 // export const endpoint = `https://seahorse-app-jnntp.ondigitalocean.app/api/graphql`;
-export const prodEndpoint = `https://api.gogeturgroceries.com/api/graphql`;
-// export const prodEndpoint = `https://agile-scrubland-77787.herokuapp.com/https://api.gogeturgroceries.com/api/graphql`;
+// export const prodEndpoint = `https://api.gogeturgroceries.com/api/graphql`;
+export const prodEndpoint = `https://agile-scrubland-77787.herokuapp.com/https://api.gogeturgroceries.com/api/graphql`;
 export const perPage = 4;
