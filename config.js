4573499db3b6ca698cacba13dece9a8dfabf697a
marla294@gmail.com <marla294@gmail.com>
2022-06-09 16:38:48 -0500

    updating prod api endpoint

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-// export const endpoint = `https://seahorse-app-jnntp.ondigitalocean.app/api/graphql`;
-export const prodEndpoint = `http://localhost:3000/api/graphql`;
-// export const prodEndpoint = `https://agile-scrubland-77787.herokuapp.com/https://api.gogeturgroceries.com/api/graphql`;
-export const perPage = 4;
+export const prodEndpoint = `https://www.gogeturgroceries.com/server/api/graphql`;
