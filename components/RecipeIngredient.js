acfff1473b78fc255cf50992d00b1afe5881b0b6
marla294@gmail.com <marla294@gmail.com>
2022-06-28 15:00:28 -0500

    adding prop types to RecipeIngredient component

--- components/RecipeIngredient.js
+++ components/RecipeIngredient.js
@@ -1,12 +1,10 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import DeleteRecipeItem from './DeleteRecipeItem';
 import ListItemStyles from './styles/ListItemStyles';
 
-export default function RecipeIngredient({
-  recipeItemId,
-  ingredient,
-  quantity,
-}) {
+function RecipeIngredient({ recipeItemId, ingredient, quantity }) {
   return (
     <ListItemStyles>
       {ingredient?.photo?.image?.publicUrlTransformed ? (
@@ -32,3 +30,21 @@ export default function RecipeIngredient({
     </ListItemStyles>
   );
 }
+
+RecipeIngredient.propTypes = {
+  recipeItemId: PropTypes.string,
+  ingredient: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    units: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    photo: PropTypes.shape({
+      image: PropTypes.shape({
+        publicUrlTransformed: PropTypes.string,
+      }),
+      altText: PropTypes.string,
+    }),
+  }).isRequired,
+  quantity: PropTypes.number.isRequired,
+};
+
+export default RecipeIngredient;
