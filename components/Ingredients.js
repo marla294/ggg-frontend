02c10b9b9754466e4eed3f549de361ae422a11f2
marla294@gmail.com <marla294@gmail.com>
2023-09-18 07:07:17 -0500

    Removing sorting from IngredientsList

--- components/Ingredients.js
+++ components/Ingredients.js
@@ -6,20 +6,12 @@ import useForm from '../lib/useForm';
 import IngredientsList from './IngredientsList';
 import PleaseSignIn from './PleaseSignIn';
 import IngredientsBarStyles from './styles/IngredientsBarStyles';
-import SortByStyles from './styles/SortByStyles';
 
 export default function Ingredients() {
   const { inputs, handleChange } = useForm({
     searchTerm: '',
     sortBy: 'alphabetical',
   });
-
-  const sortOptions = [
-    { display: 'Alphabetical', value: 'alphabetical' },
-    { display: 'Aisle', value: 'aisle' },
-    { display: 'Home area', value: 'homeArea' },
-    { display: 'Store', value: 'store' },
-  ];
 
   return (
     <PleaseSignIn>
@@ -38,26 +30,6 @@ export default function Ingredients() {
             Add
           </ButtonStyles>
         </div>
-        <SortByStyles>
-          <label htmlFor="sortBy">Sort:</label>
-          <select
-            name="sortBy"
-            id="sortBy"
-            value={inputs.sortBy}
-            onChange={handleChange}
-          >
-            {sortOptions.map((option) => (
-              <option
-                value={option.value}
-                name={option.value}
-                id={option.value}
-                key={Math.random()}
-              >
-                {option.display}
-              </option>
-            ))}
-          </select>
-        </SortByStyles>
         <input
           name="searchTerm"
           id="searchTerm"
